9a3b76de197070598c778a76b7cccb56e7fc8de4
Mohamed Osoble <ossoble360@gmail.com>
2023-09-12 16:46:16 -0400

    objects training

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name : "Sam",
+    streetAddress : '11 Broadway' ,
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const copyofemployee = {...employee}
+    copyofemployee[key] = value
+    return(copyofemployee)
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee.streetAddress = "12 Broadway" ;
+    return(employee)
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+   delete newEmployee[key] 
+    return(newEmployee)
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    
+    delete employee[key] 
+     return(employee)
+
+}
